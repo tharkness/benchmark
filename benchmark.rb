6c2c8dd36cd60cce46b9d5031ec2808efe4686b5
vagrant <vagrant@lighthouse-labs>
2016-02-04 19:42:12 +0000

    benchmark created

---
+++ benchmark.rb
@@ -0,0 +1,16 @@
+# require 'byebug'
+# byebug
+def benchmark
+  start_time = Time.now
+  yield
+  end_time = Time.now
+  return end_time - start_time 
+end
+ 
+# Be careful, pasting this into IRB will take a long time to print.
+# It's a loooong string. :)
+long_string = "apple"*10000000
+ 
+running_time = benchmark { long_string.reverse }
+
+puts "string.reverse took #{running_time} seconds to run"
